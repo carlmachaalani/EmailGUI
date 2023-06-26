76a728ba8ac05a9647daa35459c053a188e7d7c7
Carl Machaalani <126136339+carlmachaalani@users.noreply.github.com>
2023-06-26 01:03:51 -0400

    Replaced code at lines 15 to 21 in file EmailGUI/ViewModels/EmailViewModel.swift

--- EmailGUI/ViewModels/EmailViewModel.swift
+++ EmailGUI/ViewModels/EmailViewModel.swift
@@ -12,10 +12,13 @@ class EmailViewModel: ObservableObject {
     @Published private var emailModel: EmailModel = EmailModel()
 @Published var searchTerm: String = ""
     
-    var emails: [Email] {
-        emailModel.emails
+var emails: [Email] {
+    return emailModel.emails.filter { email in
+      return email.subject.contains(self.searchTerm) || email.sender.contains(self.searchTerm) || email.content.contains(self.searchTerm)
     }
+}
+
 func filterMails(searchTerm: String) {
     self.searchTerm = searchTerm
-}
+    objectWillChange.send()
 }
