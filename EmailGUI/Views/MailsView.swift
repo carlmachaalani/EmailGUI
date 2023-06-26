fdd0a045c7c779766bb75542f4b5aa53b1d57718
Carl Machaalani <126136339+carlmachaalani@users.noreply.github.com>
2023-06-26 01:03:23 -0400

    Replaced code at lines 45 to 45 in file EmailGUI/Views/MailsView.swift

--- EmailGUI/Views/MailsView.swift
+++ EmailGUI/Views/MailsView.swift
@@ -42,7 +42,7 @@ struct MailsView: View {
                     .padding(.leading, 8)
                     .foregroundColor(.gray)
                 
-TextField("Email, Name, etc...", text: $searchTerm, onEditingChanged: { _ in handleSearchTermChanged() }).onTapGesture { searchTerm = "" }
+TextField("Email, Name, etc...", text: $searchTerm).onChange(of: searchTerm, perform: { value in handleSearchTermChanged()}).onTapGesture { searchTerm = "" }
                     .foregroundColor(.gray)
 
                 Image(systemName: "mic.fill")
